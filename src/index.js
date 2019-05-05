ae3f926c652244c0325bcaeb548866aeabeb87f3
Ivelina <ivs.yordanova@gmail.com>
2019-05-05 09:17:06 +0100

    blog post sim

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 // Import react and reatdom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = function () {
-    return <div>Hi there!</div>;
+   return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                Are you sure you want to do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00PM"
+                    content="I like the subject"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 6:15PM"
+                    content="I like the topic"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    )
 }
 // Take react component and show on the screen
 ReactDOM.render(
